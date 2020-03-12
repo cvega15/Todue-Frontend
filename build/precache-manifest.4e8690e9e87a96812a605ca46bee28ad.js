8ec6439d81d6f2b65aa11e738bbdea8c83f80494
carlo <carlovega20@gmail.com>
2020-03-12 07:28:47 -0700

    main commit

---
+++ build/precache-manifest.4e8690e9e87a96812a605ca46bee28ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54da345bc0e7b34ff98eed8c40c2cf6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d48505ec36aa50debf08",
+    "url": "/static/css/main.363854c9.chunk.css"
+  },
+  {
+    "revision": "b6dae7638de984c1f392",
+    "url": "/static/js/2.2675439d.chunk.js"
+  },
+  {
+    "revision": "d48505ec36aa50debf08",
+    "url": "/static/js/main.33c724d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "566474e866594bfd5ca863869eaacbf0",
+    "url": "/static/media/app2.566474e8.png"
+  }
+]);
